2439e430fe831d4d2ff68e9956f16c145b4a6e52
IPHOUSES\piyapong.s <ipone.official@gmail.com>
2024-08-20 15:11:09 +0700

    ::edit get username localStorage

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,7 +1,6 @@
 <template>
   <v-toolbar color="#007fc4" class="px-5 py-3" dark v-if="showDrawer">
-    <img src="
-https://webapps.ip-one.com/IP1Career/static/logo-ipone-white.png" width="60vh" height="60vh" />
+    <img src="https://webapps.ip-one.com/IP1Career/static/logo-ipone-white.png" width="60vh" height="60vh" />
 
     <v-spacer></v-spacer>
     <v-toolbar-title v-if="showUser" class="name-fl">{{ us }}</v-toolbar-title>
@@ -14,8 +13,9 @@ https://webapps.ip-one.com/IP1Career/static/logo-ipone-white.png" width="60vh" h
       </template>
       <v-list>
         <v-list-item v-for="(item, index) in items" :key="index" @click="item.click">
-          <v-list-item-title><v-icon style="padding-right: 5px;" color="#007bc4">{{ item.icon }}</v-icon>{{ item.title
-            }}</v-list-item-title>
+          <v-list-item-title>
+            <v-icon style="padding-right: 5px;" color="#007bc4">{{ item.icon }}</v-icon>{{ item.title }}
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
@@ -23,67 +23,51 @@ https://webapps.ip-one.com/IP1Career/static/logo-ipone-white.png" width="60vh" h
   <v-divider :thickness="18" color="#f8c849" class="border-opacity-100"></v-divider>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          icon: "mdi-logout",
-          href: "#",
-          title: "Log Out",
-          click: this.logout,
-        },
-      ],
-      us: ""
-    };
-  },
+<script setup>
+import { ref, watchEffect, onMounted, onUnmounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
-  mounted() {
-    this.getName();
+const us = ref(localStorage.getItem('userData'));
+const items = ref([
+  {
+    icon: "mdi-logout",
+    href: "#",
+    title: "Log Out",
+    click: logout,
   },
+]);
 
-  computed: {
-    showDrawer() {
-      const path = this.$route.path;
-      return path === "/about" || "/"; // กำหนดเส้นทางที่ต้องการแสดง
-    },
-    showUser() {
-      const path = this.$route.path;
-      return path === "/about"; // กำหนดเส้นทางที่ต้องการแสดง
-    },
-  },
+const route = useRoute();
+const router = useRouter();
 
-  methods: {
-    getName() {
-      this.us = localStorage.getItem("userData");
-    },
-    logout() {
-      localStorage.removeItem("userData");
-      localStorage.removeItem("jwtToken");
-      localStorage.removeItem("userId");
-      this.us = "";
-      this.$router.push("/");
-    },
-    startTimer() {
-      this.updateTime();
-      this.timer = setInterval(() => {
-        this.countdown--;
-        this.updateTime();
-        if (this.countdown <= 0) {
-          clearInterval(this.timer);
-          this.isActive = false;
-        }
-      }, 1000);
-    },
-    updateTime() {
-      const minutes = Math.floor(this.countdown / 60);
-      const seconds = this.countdown % 60;
-      this.timeLeft.minutes = minutes.toString().padStart(2, '0');
-      this.timeLeft.seconds = seconds.toString().padStart(2, '0');
-    }
-  },
-};
+const showDrawer = ref(route.path === "/about" || route.path === "/");
+const showUser = ref(route.path === "/about");
+
+function updateUserData() {
+  us.value = localStorage.getItem("userData");
+}
+
+watchEffect(() => {
+  showDrawer.value = route.path === "/about" || route.path === "/";
+  showUser.value = route.path === "/about";
+  updateUserData();
+});
+
+onMounted(() => {
+  window.addEventListener('storage', updateUserData);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('storage', updateUserData);
+});
+
+function logout() {
+  localStorage.removeItem("userData");
+  localStorage.removeItem("jwtToken");
+  localStorage.removeItem("userId");
+  us.value = "";
+  router.push("/");
+}
 </script>
 
 <style scoped>
